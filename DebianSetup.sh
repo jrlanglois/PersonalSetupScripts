e564bb28f58c104f936f5e77c74269fdfe3bc44c
Joël R. Langlois <joel.r.langlois@gmail.com>
2020-11-19 15:25:14 -0500

    Update DebianSetup.sh

--- DebianSetup.sh
+++ DebianSetup.sh
@@ -6,6 +6,8 @@ sudo apt update && sudo apt upgrade && sudo apt dist-upgrade
 sudo apt install -y openjdk-8-jdk openjdk-8-jre build-essential dkms jackd qjackctl automake apt-transport-https ninja-build subversion git curl python python3 gcc-multilib g++-multilib genisoimage xorriso libavahi-compat-libdnssd-dev zlib1g-dev xclip libssl-dev libcurl4-openssl-dev checkinstall zlib1g-dev libexpat1-dev sshpass libssl-dev libasound2-dev libx11-dev libxinerama-dev libxext-dev libfreetype6-dev libwebkit2gtk-4.0-dev libglu1-mesa-dev
 sudo apt install -y multiarch-support # Note that this will exclusively work on Ubuntu!
 
+git config --global core.editor "code --wait"
+
 # Start off from the home folder.
 
 cd ~/
